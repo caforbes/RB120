cac16a36c7671e47ba366da745ef7c4c1ee6c763
caforbes <for.c.forbes@gmail.com>
2020-10-08 11:40:48 -0700

    RPS walkthrough

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -2,45 +2,127 @@ class RPSGame
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def play
     display_welcome_message
-    human.choose
-    computer.choose
-    display_winner
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
     display_goodbye_message
   end
-end
 
-class Player
-  def initialize
-    # maybe a "name"? what about a "move"?
+  def display_welcome_message
+    puts "Hi, #{human.name}! Welcome to Rock, Paper, Scissors!"
+    puts "Today you are playing against #{computer.name}."
   end
 
-  def choose
+  def display_goodbye_message
+    puts "Goodbye, #{human.name}! Thanks for playing Rock, Paper, Scissors!"
+  end
 
+  def display_winner
+    puts "You chose #{human.move}!"
+    puts "#{computer.name} chose #{computer.move}!"
+
+    case
+    when human.move.defeats?(computer.move)
+      puts "You won!"
+    when computer.move.defeats?(human.move)
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include?(answer.downcase)
+      puts "Sorry, please enter y or n."
+    end
+    answer == 'y'
   end
 end
 
 class Move
-  def initialize
-    # seems like we need something to keep track
-    # of the choice... a move object can be "paper", "rock" or "scissors"
+  VALUES = ['rock', 'paper', 'scissors']
+
+  def initialize(value)
+    @value = value
+  end
+
+  def to_s
+    @value
+  end
+
+  def defeats?(other)
+    if rock? then other.scissors?
+    elsif scissors? then other.paper?
+    elsif paper? then other.rock?
+    end
+  end
+
+  def rock?
+    @value == Move::VALUES[0] # rock
+  end
+
+  def paper?
+    @value == Move::VALUES[1] # paper
+  end
+
+  def scissors?
+    @value == Move::VALUES[2] # scissors
   end
 end
 
-class Rule
+class Player
+  attr_accessor :move, :name
+
   def initialize
-    # not sure what the "state" of a rule object should be
+    set_name
   end
 end
 
-# not sure where "compare" goes yet
-def compare(move1, move2)
+class Human < Player
+  def set_name
+    n = nil
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, please enter a value."
+    end
+    self.name = n
+  end
 
+  def choose
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors:"
+      choice = gets.chomp
+      break if Move::VALUES.include?(choice)
+      puts "Sorry, invalid choice."
+    end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = ["Computer", "R2D2", "C3P0"].sample
+  end
+
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
+  end
 end
 
 RPSGame.new.play
