577603411e09aa5b8257615def22cd9225a4435d
Clarissa Forbes <for.c.forbes@gmail.com>
2020-10-21 17:55:08 +0000

    TTT detect winner

--- lesson_5/ttt.rb
+++ lesson_5/ttt.rb
@@ -18,10 +18,10 @@ class TTTGame
 
     loop do
       human_moves
-      break if board.full?
+      break if board.someone_won? || board.full?
       # break if someone_won? || board_full?
       computer_moves
-      break if board.full?
+      break if board.someone_won? || board.full?
       # break if someone_won? || board_full?
 
       display_board
@@ -45,21 +45,29 @@ class TTTGame
     puts "You are #{human.marker}. Computer is #{computer.marker}."
 
     puts "     |     |"
-    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |    #{board.get_square_at(3)}"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
     puts "     |     |"
     puts "-----+-----+-----"
     puts "     |     |"
-    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |    #{board.get_square_at(6)}"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
     puts "     |     |"
     puts "-----+-----+-----"
     puts "     |     |"
-    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |    #{board.get_square_at(9)}"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
     puts "     |     |"
   end
 
   def display_result
     display_board
-    puts "The board is full!"
+
+    case board.detect_winner
+    when HUMAN_MARKER
+      puts "You won!"
+    when COMPUTER_MARKER
+      puts "Computer won!"
+    else
+      puts "It's a tie!"
+    end
   end
 
   def human_moves
@@ -82,6 +90,10 @@ class TTTGame
 end
 
 class Board
+  WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                  [[1, 5, 9], [3, 5, 7]] # diags
+
   def initialize
     @squares = {}
     (1..9).each { |key| @squares[key] = Square.new }
@@ -102,6 +114,36 @@ class Board
   def full?
     unmarked_keys.empty?
   end
+
+  def someone_won?
+    !!detect_winner
+  end
+
+  def detect_winner # returns winning marker or nil
+    WINNING_LINES.each do |line|
+      current_markers = @squares.values_at(*line).map(&:marker)
+
+      if current_markers.all? {|marker| marker == TTTGame::HUMAN_MARKER }
+        return TTTGame::HUMAN_MARKER
+      elsif current_markers.all? {|marker| marker == TTTGame::COMPUTER_MARKER }
+        return TTTGame::COMPUTER_MARKER
+      end
+      # if count_human_marker(current_squares) == 3
+      #   return TTTGame::HUMAN_MARKER
+      # elsif count_computer_marker(current_squares) == 3
+      #   return TTTGame::COMPUTER_MARKER
+      # end
+    end
+    nil
+  end
+
+  # def count_human_marker(squares)
+  #   squares.collect(&:marker).count(TTTGame::HUMAN_MARKER)
+  # end
+
+  # def count_computer_marker(squares)
+  #   squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
+  # end
 end
 
 class Square
