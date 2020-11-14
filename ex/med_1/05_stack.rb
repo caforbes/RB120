9f2857c8c4043031244f68ede93e5f349abe5859
Clarissa Forbes <for.c.forbes@gmail.com>
2020-11-14 17:05:58 +0000

    ex med1: finished 5 (stack machine)

--- ex/med_1/05_stack.rb
+++ ex/med_1/05_stack.rb
@@ -1,4 +1,10 @@
+class MinilangError < StandardError ; end
+class BadTokenError < MinilangError ; end
+class EmptyStackError < MinilangError ; end
+
 class Minilang
+  VALID = %w(PUSH ADD SUB MULT DIV MOD POP PRINT)
+
   def initialize(program)
     @stack = []
     @register = 0
@@ -6,80 +12,100 @@ class Minilang
   end
 
   def eval
-    @program.each do |command|
-      case validate(command)
-      when :update then update(command.to_i)
-      else send(validate(command))
-      end
-    end
-    # for each item in program
-    # identify if it is number, command, or invalid
-      # if number, add to stack
-      # if command, run command
-      # if invalid, raise error
+    @program.each { |token| evaluate_token(token) }
+  rescue MinilangError => e
+    puts e.message
   end
 
   private
 
-  VALID = %w(PUSH ADD SUB MULT DIV MOD POP PRINT)
-
   attr_reader :stack, :register
 
-  def update(n)
-
+  def evaluate_token(token)
+    if VALID.include?(token)
+      command = token.downcase.to_sym
+      check_stack unless [:print, :push].include?(command)
+      send(command)
+    elsif token.to_i.to_s == token
+      @register = token.to_i
+    else
+      raise BadTokenError, "Invalid token: #{command}"
+    end
   end
 
-  def push
-
+  def check_stack
+    if stack.size < 1
+      raise EmptyStackError, "Empty stack!"
+    end
   end
 
   def print
     puts register
   end
 
-  def validate(command)
-    if VALID.include?(command)
-      command.downcase.to_sym
-    elsif command.to_i.to_s == command
-      :update
-    else
-      raise ArgumentError, "Invalid token: #{command}"
-    end
+  def push
+    stack << register
+  end
+
+  def pop
+    @register = stack.pop
+  end
+
+  # all of these could be changed to pop instead of stack.pop -- eval on right side -- efficient, but ambiguous
+  def add
+    @register += stack.pop
+  end
+
+  def sub
+    @register -= stack.pop
+  end
+
+  def mult
+    @register *= stack.pop
+  end
+
+  def div
+    @register /= stack.pop
+  end
+
+  def mod
+    @register %= stack.pop
+  end
+
+  def num(n)
+    @register = n
   end
 end
-
-# split string into specific words
-# interpret if word is number, command, or invalid
 
 Minilang.new('PRINT').eval
 # 0
 
-# Minilang.new('5 PUSH 3 MULT PRINT').eval
-# # 15
+Minilang.new('5 PUSH 3 MULT PRINT').eval
+# 15
 
-# Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
-# # 5
-# # 3
-# # 8
+Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
+# 5
+# 3
+# 8
 
-# Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
-# # 10
-# # 5
+Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
+# 10
+# 5
 
-# Minilang.new('5 PUSH POP POP PRINT').eval
-# # Empty stack!
+Minilang.new('5 PUSH POP POP PRINT').eval
+# Empty stack!
 
-# Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
-# # 6
+Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
+# 6
 
-# Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
-# # 12
+Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
+# 12
 
-# Minilang.new('-3 PUSH 5 XSUB PRINT').eval
-# # Invalid token: XSUB
+Minilang.new('-3 PUSH 5 XSUB PRINT').eval
+# Invalid token: XSUB
 
-# Minilang.new('-3 PUSH 5 SUB PRINT').eval
-# # 8
+Minilang.new('-3 PUSH 5 SUB PRINT').eval
+# 8
 
-# Minilang.new('6 PUSH').eval
-# # (nothing printed; no PRINT commands)
+Minilang.new('6 PUSH').eval
+# (nothing printed; no PRINT commands)
