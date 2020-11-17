709712759ac34a1f7fa246831bf87dc48cbce773
caforbes <for.c.forbes@gmail.com>
2020-11-17 16:21:34 -0700

    edited study snippet

--- study.rb
+++ study.rb
@@ -1,17 +1,19 @@
 class MyClass
-  attr_accessor :variable
-
-  def set_var1
-    variable = 1
+  def initialize
+    @name = "Default"
   end
 
-  def set_var2
-    self.variable = 1
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
   end
 end
 
-obj = MyClass.new
-obj.set_var1
-puts obj.variable # => nil
-obj.set_var2
-puts obj.variable # => 1
+artemis = MyClass.new
+
+puts artemis.name
+artemis.name = "Artemis"
+puts artemis.name
