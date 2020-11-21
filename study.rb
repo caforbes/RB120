8f9b4696d6d8c84939ede1f4572cf8dc1f94cf87
caforbes <for.c.forbes@gmail.com>
2020-11-20 17:48:29 -0700

    edited study snippet

--- study.rb
+++ study.rb
@@ -1,19 +1,33 @@
-class MyClass
-  def initialize
-    @name = "Default"
-  end
+# module Swims
+#   SWIMS_IN = "H20"
 
-  def name
-    @name
-  end
+#   def enable_swimming
+#     @can_swim = true
+#   end
 
-  def name=(new_name)
-    @name = new_name
+#   def swim
+#     puts "blub blub"
+#   end
+# end
+
+# class Penguin
+#   include Swims
+
+#   def initialize
+#     add_swimmer
+#   end
+# end
+
+# plucky = Penguin.new
+# puts Swims.num_swimmers
+# puts Swims::SWIMS_IN
+
+module Roman
+  ALPHABET = ('A'..'Z').to_a
+
+  def self.numeral_convert(roman)
+    12  # code to make roman-arabic numeral convert
   end
 end
 
-artemis = MyClass.new
-
-puts artemis.name
-artemis.name = "Artemis"
-puts artemis.name
+puts Roman.numeral_convert('XII')
