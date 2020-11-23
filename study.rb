806c092da535a555c352da0d08a4c597371201fa
caforbes <for.c.forbes@gmail.com>
2020-11-23 13:59:19 -0700

    edited study snippet

--- study.rb
+++ study.rb
@@ -1,33 +1,15 @@
-# module Swims
-#   SWIMS_IN = "H20"
-
-#   def enable_swimming
-#     @can_swim = true
-#   end
-
-#   def swim
-#     puts "blub blub"
-#   end
-# end
-
-# class Penguin
-#   include Swims
-
-#   def initialize
-#     add_swimmer
-#   end
-# end
-
-# plucky = Penguin.new
-# puts Swims.num_swimmers
-# puts Swims::SWIMS_IN
-
-module Roman
-  ALPHABET = ('A'..'Z').to_a
-
-  def self.numeral_convert(roman)
-    12  # code to make roman-arabic numeral convert
-  end
+module Mailable
+  attr_accessor :mailing_address
 end
 
-puts Roman.numeral_convert('XII')
+class Building
+end
+
+class Library < Building
+  include Mailable
+end
+
+lib = Library.new
+p lib.class.ancestors
+
+p Module.ancestors
