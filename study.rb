368879832171e06a38aee7ef12e3690731c06f52
Clarissa Forbes <for.c.forbes@gmail.com>
2020-11-28 20:53:21 +0000

    more study file edits

--- study.rb
+++ study.rb
@@ -1,15 +1,17 @@
-module Mailable
-  attr_accessor :mailing_address
+class Fish
+  attr_reader :length
+
+  def initialize(length)
+    @length = length
+  end
+
+  def ==(other)
+    length == other.length
+  end
 end
 
-class Building
-end
+a = Fish.new(20)
+b = Fish.new(20)
 
-class Library < Building
-  include Mailable
-end
-
-lib = Library.new
-p lib.class.ancestors
-
-p Module.ancestors
+puts a == b   # => true
+puts a === b   # => true
